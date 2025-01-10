ab18bbc887ee4b6c309db495d52aef2190062a1a
miguelAvatr <macollantes@avatar-global.com>
2025-01-09 19:10:11 -0500

    NEWARQ - tienda eventos

--- controllers/StoreEvent.js
+++ controllers/StoreEvent.js
@@ -9,77 +9,37 @@ const ITEMS_POR_PAGINA = 8;
 
 exports.getDetalleEventoTienda = async (req, res, next) => {
     const idEvento = req.params.idEvento;
-
-    var autenticado  = req.session.autenticado;
-    var dataUser     = null;
     var isWishlisted = false;
 
-    if (autenticado) {
-        dataUser    = req.session.usuario;
-    }
+    Evento.findById(idEvento).populate({
+        path: 'catalogoEntradas',
+        populate: {
+            path: 'tipoEntrada',
+            select: 'nombre'
+        }
+    })
+    .then(evento => {
+        if (!evento) {
+            const error = new Error('No se encontró un evento con el id especificado.');
+            error.statusCode = 404;
+            throw error;
+        }
 
-    try {
-        Evento.findById(idEvento).populate({
-            path: 'catalogoEntradas',
-            populate: {
-                path: 'tipoEntrada',
-                select: 'nombre'
-            }
-        })
-        .then(evento => {
-            if (!evento) {
-                return res.status(404).render('error', {
-                    mensaje: "Evento no encontrado."
-                });
-            }
+        const fechaFormateada = format(evento.fecha, 'dd/MM/yyyy');
 
-            const fechaFormateada = format(evento.fecha, 'dd/MM/yyyy');
-
-            if (!autenticado) {
-                return res.render('tienda/events/detalle-evento', {
-                    titulo          : evento.nombre,
-                    evento          : evento,
-                    autenticado     : false,
-                    isAdmiUser      : false,
-                    isWishlisted    : false,
-                    usuario         : null,
-                    fechaFormateada : fechaFormateada, 
-                    opcion          : 'detalleEvento'
-                })
-            } else {
-                Usuario.findById(dataUser._id)
-                .then(usuario => {
-
-                    if (usuario.eventosFavoritos && usuario.eventosFavoritos.length > 0) {
-                        const index = usuario.eventosFavoritos.findIndex(evento => evento._id == idEvento);
-
-                        isWishlisted = index > -1;                        
-                    }
-
-                    var isAdmiUser = dataUser.isAdmin;
-
-                    return res.render('tienda/events/detalle-evento', {
-                        titulo          : evento.nombre,
-                        evento          : evento,
-                        autenticado     : autenticado,
-                        isAdmiUser      : isAdmiUser,
-                        isWishlisted    : isWishlisted,
-                        usuario         : dataUser,
-                        fechaFormateada : fechaFormateada, 
-                        opcion          : 'detalleEvento'
-                    })
-                })
-            }        
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    } catch (e) {
-        console.log(e);
-        const error = new Error(e);
-        error.httpStatusCode = 500;
-        return next(error);
-    }
+        res.status(200).json({
+            evento          : evento,
+            mensaje         : 'Se encontró el evento exitosamente.',
+            isWishlisted    : isWishlisted,
+            fechaFormateada : fechaFormateada, 
+        });     
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
 }
 
 exports.getListadoEventos = async (req, res, next) => {
@@ -96,13 +56,6 @@ exports.getListadoEventos = async (req, res, next) => {
     const citiesOptions = eventsHelper.getCitiesOptions();
 
     const filter = {};
-
-    var autenticado  = req.session.autenticado;
-    var dataUser     = null;
-
-    if (autenticado) {
-        dataUser   = req.session.usuario;
-    }
 
     if (searchTerm) {
         filter.nombre = { $regex: searchTerm, $options: 'i' };
@@ -161,32 +114,30 @@ exports.getListadoEventos = async (req, res, next) => {
         const querySinPagina = { ...req.query };
         delete querySinPagina.pagina;
 
-        res.render('tienda/events/listado-eventos', {
-            eventos       : eventosFormateados,
-            categorias    : categorias,
-            citiesOptions : citiesOptions,
-            titulo        : "Encuentra eventos", 
-            opcion        : 'listadoEventos',
-            autenticado   : autenticado,
-            usuario       : autenticado ? dataUser : null,
-            precioMinSeleccionado   : priceMin,
-            precioMaxSeleccionado   : priceMax,
-            categoriaSeleccionada   : categorySelected,
-            ciudadSeleccionada      : citySelected,
-            fechaInicioSeleccionada : dateStart, 
-            fechaFinSeleccionada    : dateEnd,
-            direccionActual      : '/tienda/buscar-eventos?' + new URLSearchParams(querySinPagina).toString(),
-            paginaActual         : pagina,
-            tienePaginaSiguiente : ITEMS_POR_PAGINA * pagina < nroEventos,
-            tienePaginaAnterior  : pagina > 1,
-            paginaSiguiente      : pagina + 1,
-            paginaAnterior       : pagina - 1,
-            ultimaPagina         : Math.ceil(nroEventos / ITEMS_POR_PAGINA)
-        });
+        res.json(
+            {
+                eventos                 : eventosFormateados,
+                mensaje                 : 'Procesado exitosamente',
+                precioMinSeleccionado   : priceMin,
+                precioMaxSeleccionado   : priceMax,
+                categoriaSeleccionada   : categorySelected,
+                ciudadSeleccionada      : citySelected,
+                fechaInicioSeleccionada : dateStart, 
+                fechaFinSeleccionada    : dateEnd,
+                direccionActual      : '/tienda/buscar-eventos?' + new URLSearchParams(querySinPagina).toString(),
+                paginaActual         : pagina,
+                tienePaginaSiguiente : ITEMS_POR_PAGINA * pagina < nroEventos,
+                tienePaginaAnterior  : pagina > 1,
+                paginaSiguiente      : pagina + 1,
+                paginaAnterior       : pagina - 1,
+                ultimaPagina         : Math.ceil(nroEventos / ITEMS_POR_PAGINA)
+            }
+        );        
     })
     .catch(err => {
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);        
     });
 }
--- routes/tienda.js
+++ routes/tienda.js
@@ -7,7 +7,7 @@ const storeEventController = require('../controllers/StoreEvent');
 const purchaseController = require('../controllers/Purchase');
 
 router.get ('/', homeController.getIndex);
-router.get('/detalle-evento/:idEvento', storeEventController.getDetalleEventoTienda);
+router.get('/eventos/:idEvento', storeEventController.getDetalleEventoTienda);
 
 router.post('/realizar-compra', isAuth, purchaseController.postRealizarCompra);
 router.get('/detalle-compra/:idCompra', isAuth, purchaseController.getDetalleCompra);
