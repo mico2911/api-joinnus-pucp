ccdc9db6b93b5c2557da7e7d32e507734623e89d
miguelAvatr <macollantes@avatar-global.com>
2025-02-09 20:55:09 -0500

    tesst

--- controllers/Home.js
+++ controllers/Home.js
@@ -8,7 +8,7 @@ exports.getHomeEvents = (req, res, next) => {
 
     Evento
     .find({ fecha: { $gte: today } })
-    .limit(3)
+    .limit(2)
     .then(eventos => {
 
         const eventosConFechaParsed = eventos.map(evento => {
