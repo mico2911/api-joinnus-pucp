220057c87878697756bbbd9bfa27dccf81116810
miguelAvatr <macollantes@avatar-global.com>
2025-02-10 19:44:25 -0500

    cookie

--- controllers/Login.js
+++ controllers/Login.js
@@ -43,7 +43,11 @@ exports.postIngresar = (req, res, next) => {
 
             const token = jwt.sign(userData, CIFRADO_LLAVE, {expiresIn : '2h'});
 
-            res.cookie('token', token);
+            res.cookie('token', token, {
+              secure: true,
+              sameSite: 'None',
+              maxAge: 3600000,
+            });
 
             res.status(200).json({
                 mensaje   : 'Sesión iniciada con éxito',
